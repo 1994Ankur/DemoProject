b6b1c1a96ca60a44e45a836f2c1a989afe655629
1994Ankur <ankurnarwal79888@gmail.com>
2021-02-06 12:20:24 +0530

    Added Ankur NArwal

---
+++ src/com/ptc/Main.java
@@ -0,0 +1,10 @@
+package com.ptc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ankur Narwal");
+        System.out.println("Ankur Narwal 2");
+	// write your code here
+    }
+}
